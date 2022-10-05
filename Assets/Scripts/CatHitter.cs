207e599be1af090843438a8a0f0697804a0d6399
Mahmoud0090 <mmh.00090@gmail.com>
2022-10-05 03:00:17 +0300

    Score and 3D score

--- Assets/Scripts/CatHitter.cs
+++ Assets/Scripts/CatHitter.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CatHitter : MonoBehaviour
 {
+    public GameObject textScore;
+    public TextMesh textScore3D;
     public int explosionForce = 150;
     public int explosionRadius = 25;
     public GameObject[] hitParticles;
@@ -14,9 +17,19 @@ public class CatHitter : MonoBehaviour
             print("Ok");
             hit.gameObject.tag = "Hitted";
             Rigidbody obj_rb = hit.gameObject.GetComponent<Rigidbody>();
+            HittableObject ho = hit.gameObject.GetComponent<HittableObject>();
             obj_rb.isKinematic = false;
             obj_rb.AddExplosionForce(explosionForce, transform.position + Vector3.down, explosionRadius);
             Instantiate(hitParticles[Random.Range(0, hitParticles.Length)], hit.transform.position, Quaternion.identity);
+            int newScore = int.Parse(textScore.GetComponent<TextMeshProUGUI>().text) + ho.points;
+            textScore.GetComponent<TextMeshProUGUI>().text = newScore.ToString();
+            textScore3D.text = "+" + ho.points;
+            Invoke("ResetText3D", 0.5f);
         }
+    }
+
+    public void ResetText3D()
+    {
+        textScore3D.text = "";
     }
 }
